8428167f95df38561c77c85580b2f363818edc29
gilangp1 <gilangprakoso37@gmail.com>
2020-11-05 15:59:41 +0700

    membuat table dari app.js

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ function prayerTimes(latitude, longitude){
    .then(response => response.json())
    .then(function(response){
 
-      let date    = new Date();
-      let today   = date.getDate() - 1;
-   
-      // console.log(response.data[today]);
+      let date       = new Date();
+      let today      = date.getDate() - 1;
+      let data       = response.data[today].timings;
+      console.log(response.data)
+      let app        = document.getElementById('app');
+      let table      = document.createElement('table');
+      let tableTbody = document.createElement('tbody');
+
+      for(i in data){
+         let row        = tableTbody.insertRow();
+         let name       = row.insertCell(0);
+         let time       = row.insertCell(1);
+         name.innerHTML = i;
+         time.innerHTML = data[i];
+         tableTbody.appendChild(row);
+      }
+      table.appendChild(tableTbody);
+      app.appendChild(table);
+      
    });
 }
 
